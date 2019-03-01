818d9b1e0981c59f6cb811ed3d448452d3116de9
iamwenz <47152031+iamwenz@users.noreply.github.com>
2019-03-01 22:51:28 +0800

    Update results.php

--- results.php
+++ results.php
@@ -4,14 +4,15 @@
   <meta charset="UTF-8">
   <title>E15 P2</title>
   <link href="p2.css" rel="stylesheet">
-  <?php require "logic.php"; ?>
+  <?php require "logic.php";
+  ?>
 </head>
 
 <body>
   <div class="wrapper">
     <div class="title">Basal Metabolic Rate</div>
     <div class="formBox">
-      <p>Hello <b><?php echo $name; ?></b>, your BMR is <b><?php echo $bmr; ?></b></p>
+      <p>Hello <b><?php echo $bmrForm->name; ?></b>, your BMR is <b><?php echo $bmrForm->bmr; ?></b></p>
 
     </div>
     <footer>
