76e57d751f2a01ecbe5a05c1df646617fa2a4f3b
iamwenz <47152031+iamwenz@users.noreply.github.com>
2019-03-01 22:50:05 +0800

    Update logic.php

--- logic.php
+++ logic.php
@@ -1,10 +1,31 @@
 <?php
 
-$name=$_POST["name"];
-$bmr="";
-if ($_POST["gender"] == "Male") {
-    $bmr= 88.362 + (13.75 * $_POST["weight"]) + (5.003 * $_POST["height"]) - (5.677 * $_POST["age"]);
 
-} else if ($_POST["gender"] == "Female"){
-    $bmr= 447.593 + (9.247 * $_POST["weight"]) + (3.098 * $_POST["height"]) - (4.330 * $_POST["age"]);
+// def of an object by defining bmrform
+class bmrForm {
+	public $name;
+	public $gender;
+	public $height;
+	public $weight;
+	public $age;
+	public $bmr;
+
+	public function __construct($name, $gender, $height, $weight, $age) {
+		$this->name = $name;
+		$this->gender = $gender;
+		$this->height = $height;
+		$this->weight = $weight;
+		$this->age = $age;
+
+		if($gender == 'Male'){
+			$this->bmr= 88.362 + (13.75 * $weight) + (5.003 * $height) - (5.677 * $age);
+		} else if($gender == "Female"){
+			$this->bmr= 447.593 + (9.247 * $weight) + (3.098 * $height) - (4.330 * $age);
+		}
+	}
+
 }
+//create an object
+$bmrForm = new bmrForm($_POST["name"],$_POST["gender"],$_POST["height"],$_POST["weight"],$_POST["age"]);
+
+?>
